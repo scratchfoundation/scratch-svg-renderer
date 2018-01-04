0bbc5d865ac9da926217a53dd3b84ac5f8c0d55b
Christopher Willis-Ford <cwillisf@media.mit.edu>
2018-01-04 11:00:45 -0800

    Update Webpack and Babel config
    These settings were merged in from another, separate attempt to separate
    the SVG rendering code from `scratch-render`.
    
    Changes:
    - Use `babel-preset-env` instead of `babel-preset-es2015`
    - Remove `.babelrc` in favor of settings in `webpack.config.js`
    - Add explicit node vs. web targets
    - Add `.editorconfig` entry for JSON files
    - Audit dependency versions and pinning

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,51 @@
+const defaultsdeep = require('lodash.defaultsdeep');
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-    devtool: 'cheap-module-source-map',
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            include: path.resolve(__dirname, 'src'),
-            options: {
-                plugins: ['transform-object-rest-spread'],
-                presets: ['es2015']
-            }
-        }]
-    },
-    entry: {
-        'scratch-svg-renderer': './src/svg-renderer.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        libraryTarget: 'commonjs2'
-    },
-    plugins: []
-        .concat(process.env.NODE_ENV === 'production' ? [
-            new webpack.optimize.UglifyJsPlugin({
-                include: /\.min\.js$/,
-                minimize: true
-            })
-        ] : [])
+const makeExport = function (targets, settings) {
+    const base = {
+        devtool: 'cheap-module-source-map',
+        module: {
+            rules: [{
+                include: path.resolve('src'),
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [['env', {targets}]]
+                }
+            }]
+        },
+        entry: {
+            'scratch-svg-renderer': './src/svg-renderer.js'
+        },
+        plugins: []
+            .concat(process.env.NODE_ENV === 'production' ? [
+                new webpack.optimize.UglifyJsPlugin({
+                    include: /\.min\.js$/,
+                    minimize: true
+                })
+            ] : [])
+    };
+
+    return defaultsdeep(base, settings);
 };
+
+module.exports = [
+    makeExport({browsers: ['last 3 versions', 'Safari >= 8', 'iOS >= 8']}, {
+        output: {
+            library: 'ScratchSVGRenderer',
+            libraryTarget: 'umd',
+            path: path.resolve('dist', 'web'),
+            filename: '[name].js'
+        }
+    }),
+    // For testing only: rendering will fail outside a browser
+    makeExport({node: true}, {
+        output: {
+            library: 'ScratchSVGRenderer',
+            libraryTarget: 'commonjs2',
+            path: path.resolve('dist', 'node'),
+            filename: '[name].js'
+        }
+    })
+];
