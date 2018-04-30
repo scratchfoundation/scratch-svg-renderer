2f4cdc2de2e1a34a3c0f77f26bc7cfd8b4cc3cad
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-04-30 12:39:45 -0400

    Standardize webpack config on format of other scratch projects

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,63 @@
-const defaultsdeep = require('lodash.defaultsdeep');
+const defaultsDeep = require('lodash.defaultsdeep');
 const path = require('path');
-const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const makeExport = function (targets, settings) {
-    const base = {
-        mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-        devtool: 'cheap-module-source-map',
-        module: {
-            rules: [{
-                include: path.resolve('src'),
-                test: /\.js$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [['env', {targets}]]
-                }
-            }]
-        },
-        entry: {
-            'scratch-svg-renderer': './src/index.js'
-        },
-        optimization: {
-            minimize: process.env.NODE_ENV === 'production' && /dist[\//]web/.test(settings.output.path)
-        }
-    };
-
-    return defaultsdeep(base, settings);
+const base = {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    devtool: 'cheap-module-source-map',
+    entry: {
+        'scratch-svg-renderer': './src/index.js'
+    },
+    module: {
+        rules: [{
+            include: path.resolve('src'),
+            test: /\.js$/,
+            loader: 'babel-loader',
+            options: {
+                presets: [['env', {targets: {}}]]
+            }
+        }]
+    }
 };
 
 module.exports = [
-    makeExport({browsers: ['last 3 versions', 'Safari >= 8', 'iOS >= 8']}, {
+    defaultsDeep({}, base, {
         output: {
             library: 'ScratchSVGRenderer',
             libraryTarget: 'umd',
             path: path.resolve('dist', 'web'),
             filename: '[name].js'
+        },
+        module: {
+            rules: [{
+                options: {
+                    presets: [['env', {targets: {browsers: ['last 3 versions', 'Safari >= 8', 'iOS >= 8']}}]]
+                }
+            }]
+        },
+        optimization: {
+            minimize: process.env.NODE_ENV === 'production'
         }
     }),
     // For testing only: many features will fail outside a browser
-    makeExport({node: true, uglify: true}, {
+    defaultsDeep({}, base, {
         output: {
             library: 'ScratchSVGRenderer',
             libraryTarget: 'commonjs2',
             path: path.resolve('dist', 'node'),
             filename: '[name].js'
         },
+        module: {
+            rules: [{
+                options: {
+                    presets: [['env', {targets: {node: true, uglify: true}}]]
+                }
+            }]
+        },
         performance: {
             hints: false
+        },
+        optimization: {
+            minimize: false
         }
     })
 ];
