89abe42c052243da725dbf4185f55169de4a2114
adroitwhiz <adroitwhiz@protonmail.com>
2020-06-10 17:56:03 -0400

    Revert "Move element categories into own file+correct them"
    This reverts commit 414c7d7d7d17e35d390d82e565850c7f19771df5.

--- src/transform-applier.js
+++ src/transform-applier.js
@@ -1,6 +1,5 @@
 const Matrix = require('transformation-matrix');
 const SvgElement = require('./svg-element');
-const {isPaintableElement, isContainerElement} = require('./element-categories');
 const log = require('./util/log');
 
 /**
@@ -289,6 +288,14 @@ const _transformPath = function (pathString, transform) {
     return result;
 };
 
+const GRAPHICS_ELEMENTS = ['circle', 'ellipse', 'image', 'line', 'path', 'polygon', 'polyline', 'rect', 'text', 'use'];
+const CONTAINER_ELEMENTS = ['a', 'defs', 'g', 'marker', 'glyph', 'missing-glyph', 'pattern', 'svg', 'switch', 'symbol'];
+const _isContainerElement = function (element) {
+    return element.tagName && CONTAINER_ELEMENTS.includes(element.tagName.toLowerCase());
+};
+const _isGraphicsElement = function (element) {
+    return element.tagName && GRAPHICS_ELEMENTS.includes(element.tagName.toLowerCase());
+};
 const _isPathWithTransformAndStroke = function (element, strokeWidth) {
     if (!element.attributes) return false;
     strokeWidth = element.attributes['stroke-width'] ?
@@ -507,7 +514,7 @@ const transformStrokeWidths = function (svgTag, windowRef, bboxForTesting) {
     const inherited = Matrix.identity();
 
     const applyTransforms = (element, matrix, strokeWidth, fill, stroke) => {
-        if (isContainerElement(element)) {
+        if (_isContainerElement(element)) {
             // Push fills and stroke width down to leaves
             if (element.attributes['stroke-width']) {
                 strokeWidth = element.attributes['stroke-width'].value;
@@ -594,7 +601,7 @@ const transformStrokeWidths = function (svgTag, windowRef, bboxForTesting) {
             element.setAttribute('stroke-width', _quadraticMean(matrixScale.x, matrixScale.y) * strokeWidth);
             if (fill) element.setAttribute('fill', fill);
             if (stroke) element.setAttribute('stroke', stroke);
-        } else if (isPaintableElement(element)) {
+        } else if (_isGraphicsElement(element)) {
             // Push stroke width, fill, and stroke down to leaves
             if (strokeWidth && !element.attributes['stroke-width']) {
                 element.setAttribute('stroke-width', strokeWidth);
