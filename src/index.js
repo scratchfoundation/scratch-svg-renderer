57230133f70510289782a827ccac71da5cf02461
adroitwhiz <adroitwhiz@protonmail.com>
2021-01-07 17:40:20 -0500

    Export fixupSvgString from module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const SVGRenderer = require('./svg-renderer');
 const BitmapAdapter = require('./bitmap-adapter');
+const fixupSvgString = require('./fixup-svg-string');
 const inlineSvgFonts = require('./font-inliner');
 const SvgElement = require('./svg-element');
 const convertFonts = require('./font-converter');
@@ -10,6 +11,7 @@ const convertFonts = require('./font-converter');
 module.exports = {
     BitmapAdapter: BitmapAdapter,
     convertFonts: convertFonts,
+    fixupSvgString: fixupSvgString,
     inlineSvgFonts: inlineSvgFonts,
     SvgElement: SvgElement,
     SVGRenderer: SVGRenderer
