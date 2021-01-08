57eb4a8274254ed895eac67d00b5510ebdf9afdf
adroitwhiz <adroitwhiz@protonmail.com>
2021-01-07 19:49:13 -0500

    Move SVG string fixups/loading into separate file
    The VM uses the SvgRenderer solely to convert SVGs from version 2.
    This allows the VM to simply import loadSvgString without bringing in
    the entire SvgRenderer along with it. The next commit will move toString
    to a separate file as well, so the VM's dependency on SvgRenderer can be
    entirely removed.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const SVGRenderer = require('./svg-renderer');
 const BitmapAdapter = require('./bitmap-adapter');
 const inlineSvgFonts = require('./font-inliner');
+const loadSvgString = require('./load-svg-string');
 const SvgElement = require('./svg-element');
 const convertFonts = require('./font-converter');
 // /**
@@ -11,6 +12,7 @@ module.exports = {
     BitmapAdapter: BitmapAdapter,
     convertFonts: convertFonts,
     inlineSvgFonts: inlineSvgFonts,
+    loadSvgString: loadSvgString,
     SvgElement: SvgElement,
     SVGRenderer: SVGRenderer
 };
